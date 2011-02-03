044edc7758cca8117cc545198534aebf28fbe8cf
Richard Lang <oslarathos@gmail.com>
2011-02-03 09:59:59 -0500

    added lever functions to Utils

--- src/main/java/org/langricr/mcmachina/Utils.java
+++ src/main/java/org/langricr/mcmachina/Utils.java
@@ -1,10 +1,19 @@
 package org.langricr.mcmachina;
 
 import org.bukkit.block.Block;
+import org.bukkit.material.Lever;
 import org.langricr.util.Coordinate;
 
 public class Utils {
 	public static Block getBlockAt( Coordinate coord ) {
 		return McMachina.getInstance().getServer().getWorlds()[ 0 ].getBlockAt( coord.getX(), coord.getY(), coord.getZ() );
+	}
+	
+	public static boolean isLeverPowered( Lever lever ) {
+		return lever.getData() / 8 == 1;
+	}
+	
+	public static void toggleLever( Lever lever ) {
+		lever.setData( ( byte ) ( lever.getData() ^ 8 ) );
 	}
 }
